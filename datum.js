2ae79620c14fe2cd61e0aa5989e3e59b4c0d6d27
Hajime Yamasaki Vukelic <hayavuk@gmail.com>
2017-02-15 11:15:06 +0100

    Add Week ctor implementation

--- datum.js
+++ datum.js
@@ -11,21 +11,70 @@
 
   var DAY_MS = 24 * 60 * 60 * 1000;
 
-  datum.resetTime = function (date) {
+  var resetTime = datum.resetTime = function (date) {
     return new Date(date.getFullYear(), date.getMonth(), date.getDate());
   };
 
-  datum.diff = curry(function (date1, date2) {
+  var diff = datum.diff = curry(function (date1, date2) {
     var d1 = Date.UTC(date1.getFullYear(), date1.getMonth(), date1.getDate());
     var d2 = Date.UTC(date2.getFullYear(), date2.getMonth(), date2.getDate());
-    return Math.abs(d2 - d1);
+    var diffMs = Math.abs(d2 - d1);
+    return {
+      days: diffMs / DAY_MS,
+      milliseconds: diffMs
+    };
   });
 
-  datum.shiftDate = curry(function (days, date) {
+  var shiftDate = datum.shiftDate = curry(function (days, date) {
     var d = new Date(date.valueOf());
     d.setDate(d.getDate() + days);
     return d;
   });
+
+  var range = datum.range = curry(function (n, date) {
+    if (n === 0) return [];
+    date = datum.resetTime(date);
+    var shiftBy = n < 0 ? -1 : 1;
+    var count = Math.abs(n);
+    return (function generateDates(acc, lastDate) {
+      if (acc.length === count) return acc;
+      var nextDate = shiftDate(shiftBy, lastDate);
+      return generateDates(acc.concat(nextDate), nextDate);
+    })([date], date);
+  });
+
+  var weekDayOffset = datum.weekDayOffset = curry(function (firstDayIndex, date) {
+    var shifted = date.getDay() - firstDayIndex;
+    return shifted < 0 ?
+      7 + shifted
+      : shifted;
+  });
+
+  var weekStart = datum.weekStart = curry(function (offsetFn, date) {
+    return shiftDate(0 - offsetFn(date), date);
+  });
+
+  var DEFAULTS = {
+    daysPerWeek: 7,
+    weekDayOffset: weekDayOffset(0),
+    decorate: function () {}
+  };
+
+  datum.Week = Week;
+
+  function Week(year, month, date, options) {
+    options = Object.assign({}, DEFAULTS, options || {});
+    this.start = weekStart(options.weekDayOffset, new Date(year, month, date));
+    this.days = range(options.daysPerWeek, this.start);
+    this.end = last(this.days);
+    options.decorate(this, year, month, date);
+  }
+
+  Week.factory = function (options) {
+    return function (year, month, date) {
+      return new Week(year, month, date, options);
+    };
+  };
 
   function curry(fn, args) {
     var arity = fn.length;
@@ -35,5 +84,9 @@
       if (newArgs.length >= arity) return fn.apply(null, newArgs);
       return curry(fn, newArgs);
     };
+  }
+
+  function last(a) {
+    return a[a.length - 1];
   }
 });
--- datum.test.js
+++ datum.test.js
@@ -17,18 +17,30 @@ test('resetTime returns a new Date instance', function () {
 test('diff returns difference between two dates objects in ms', function () {
   var d1 = new Date(2017, 4, 12, 12, 33, 0);
   var d2 = new Date(2017, 4, 13, 5, 18, 0);
-  expect(datum.diff(d1, d2)).toBe(86400000);
+  expect(datum.diff(d1, d2)).toEqual({
+    days: 1,
+    milliseconds: 86400000
+  });
 
   d1 = new Date(2017, 4, 12, 12, 33, 0);
   d2 = new Date(2017, 4, 14, 5, 18, 0);
-  expect(datum.diff(d1, d2)).toBe(172800000);
+  expect(datum.diff(d1, d2)).toEqual({
+    days: 2,
+    milliseconds: 172800000
+  });
 });
 
 test('diff returns absolute difference', function () {
   var d1 = new Date(2017, 4, 12, 12, 33, 0);
   var d2 = new Date(2017, 4, 14, 5, 18, 0);
-  expect(datum.diff(d1, d2)).toBe(172800000);
-  expect(datum.diff(d2, d1)).toBe(172800000);
+  expect(datum.diff(d1, d2)).toEqual({
+    days: 2,
+    milliseconds: 172800000
+  });
+  expect(datum.diff(d2, d1)).toEqual({
+    days: 2,
+    milliseconds: 172800000
+  });
 });
 
 test('diff does not care about time', function () {
@@ -43,13 +55,19 @@ test('diff is curried', function () {
   var diffFromRef = datum.diff(ref);
   var d2 = new Date(2017, 4, 14, 5, 18, 0);
   var d3 = new Date(2017, 3, 5, 8, 18, 0);
-  expect(diffFromRef(d2)).toBe(172800000);
-  expect(diffFromRef(d3)).toBe(3196800000);
+  expect(diffFromRef(d2)).toEqual({
+    days: 2,
+    milliseconds: 172800000
+  });
+  expect(diffFromRef(d3)).toEqual({
+    days: 37,
+    milliseconds: 3196800000
+  });
 });
 
 test('shiftDate returns a date object shifted by specified number of days', function () {
   var d1 = new Date(2017, 4, 3);
-  var d2 = datum.shiftDate(12, d1); 
+  var d2 = datum.shiftDate(12, d1);
   expect(d2.getFullYear()).toBe(2017);
   expect(d2.getMonth()).toBe(4);
   expect(d2.getDate()).toBe(15);
@@ -68,7 +86,7 @@ test('shiftDate can shift across years', function () {
   var d2 = datum.shiftDate(-100, d1);
   expect(d2.getFullYear()).toBe(2015);
   expect(d2.getMonth()).toBe(10);
-  expect(d2.getDate()).toBe(2);  
+  expect(d2.getDate()).toBe(2);
 });
 
 test('shiftDate is curried', function () {
@@ -83,4 +101,59 @@ test('shiftDate returns a copy of the Date object', function () {
   var d1 = new Date(2016, 1, 16);
   var d2 = datum.shiftDate(1, d1);
   expect(d1).not.toBe(d2);
+});
+
+test('range returns an array of Date objects', function () {
+  var start = new Date(2016, 2, 15);
+  var dates = datum.range(2, start);
+  expect(dates.length).toBe(2);
+  expect(dates[0].getDate()).toBe(15);
+  expect(dates[1].getDate()).toBe(16);
+});
+
+test('range returns an empty array, if number of days is 0', function () {
+  var start = new Date(2016, 2, 15);
+  var dates = datum.range(0, start);
+  expect(dates).toEqual([]);
+});
+
+test('range returns dates in reverse if number of days is less than 0', function () {
+  var start = new Date(2016, 2, 15);
+  var dates = datum.range(-2, start);
+  expect(dates[0].getDate()).toBe(15);
+  expect(dates[1].getDate()).toBe(14);
+});
+
+test('weekDayOffset returns week day index at given offset', function () {
+  var d = new Date(2017, 1, 15);  // day 3 when Sunday is 0
+  expect(datum.weekDayOffset(1, d)).toBe(2);
+  expect(datum.weekDayOffset(3, d)).toBe(0);
+  expect(datum.weekDayOffset(5, d)).toBe(5);
+});
+
+test('weekDayOffset is curried', function () {
+  var d = new Date(2017, 1, 15);  // day 3 when Sunday is 0
+  var wdo = datum.weekDayOffset(5);
+  expect(wdo(d)).toBe(5);
+});
+
+test('weekStart returns a date of the start of a week', function () {
+  var d1 = new Date(2017, 1, 15);
+  var d2 = new Date(2017, 1, 13);
+  var start1 = datum.weekStart(datum.weekDayOffset(0), d1);
+  expect(start1.getFullYear()).toBe(2017);
+  expect(start1.getMonth()).toBe(1);
+  expect(start1.getDate()).toBe(12);
+  var start2 = datum.weekStart(datum.weekDayOffset(0), d2);
+  expect(start2.getFullYear()).toBe(2017);
+  expect(start2.getMonth()).toBe(1);
+  expect(start2.getDate()).toBe(12);
+});
+
+test('weekStart is curried', function () {
+  var ws = datum.weekStart(datum.weekDayOffset(0));
+  var d1 = new Date(2017, 1, 15);
+  var d2 = new Date(2017, 1, 13);
+  expect(ws(d1).getDate()).toBe(12);
+  expect(ws(d2).getDate()).toBe(12);
 });
